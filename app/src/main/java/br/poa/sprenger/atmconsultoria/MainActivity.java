4cd551ddafb909bf669965492ece847cf0504786
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2020-07-12 18:48:31 -0300

    add enviarEmail(View view) => fab.onClick

--- app/src/main/java/br/poa/sprenger/atmconsultoria/MainActivity.java
+++ app/src/main/java/br/poa/sprenger/atmconsultoria/MainActivity.java
@@ -1,5 +1,7 @@
 package br.poa.sprenger.atmconsultoria;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -26,14 +28,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -55,5 +49,24 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    /**
+     * Usa uma intent para enviar um email apartir de um app como Gmail.
+     * Intents também podem ser usados para, por exemplo, iniciar uma nova atividade em resposta a uma ação
+     * do usuário ou iniciar um serviço para fazer o download de um arquivo em segundo plano.
+     *
+     * O fab esta dento de app_bar_main...
+     */
+    public void enviarEmail(View view){
+        //startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("40028922")));
+        //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://external-preview.redd.it/XUEQcanJ6RxwMVBdmhJLWpR6j2RvNFr2B_q-7WtCfck.jpg?auto=webp&s=f85e5a08e5066fdcc25d8b0f0982f19030ac817d")));
+        //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.br/maps/place/at-home+cafe+%E3%83%89%E3%83%B3%E3%82%AD%E5%BA%97%EF%BC%88%E3%81%82%E3%81%A3%E3%81%A8%E3%81%BB%E3%81%89%E3%83%BC%E3%82%80%E3%82%AB%E3%83%95%E3%82%A7%EF%BC%89/@35.7007698,139.7718072,166m/data=!3m1!1e3!4m12!1m6!3m5!1s0x60188c1d94792ded:0x33b2a9fd2ac534f!2zYXQtaG9tZSBjYWZlIOODieODs-OCreW6l--8iOOBguOBo-OBqOOBu-OBieODvOOCgOOCq-ODleOCp--8iQ!8m2!3d35.70083!4d139.771849!3m4!1s0x60188c1d94792ded:0x33b2a9fd2ac534f!8m2!3d35.70083!4d139.771849").se));
+        Intent enviarEmail = new Intent(Intent.ACTION_SEND);
+        enviarEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"atendimento@att.consultoria.br"});
+        enviarEmail.putExtra(Intent.EXTRA_SUBJECT, "Assunto");
+        enviarEmail.putExtra(Intent.EXTRA_TEXT, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices posuere justo ut rhoncus. Nunc egestas nulla metus.");
+        enviarEmail.setType("message/rfc822"); //Escolher de acordo com a lista... https://www.sitepoint.com/mime-types-complete-list/
+        startActivity(Intent.createChooser(enviarEmail,"Compartilhar"));
     }
 }
