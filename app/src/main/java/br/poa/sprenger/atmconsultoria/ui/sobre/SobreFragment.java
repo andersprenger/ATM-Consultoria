7ae7bb00c1ce859c07585b4dca17e8df29bd877d
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2020-07-12 19:16:22 -0300

    add sobre usando libmedyo

--- app/src/main/java/br/poa/sprenger/atmconsultoria/ui/sobre/SobreFragment.java
+++ app/src/main/java/br/poa/sprenger/atmconsultoria/ui/sobre/SobreFragment.java
@@ -9,58 +9,30 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import br.poa.sprenger.atmconsultoria.R;
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SobreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SobreFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SobreFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SobreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SobreFragment newInstance(String param1, String param2) {
-        SobreFragment fragment = new SobreFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+        Element versao = new Element();
+        versao.setTitle("Versão 07JUL20@19H12");
+        //build.gradle (module: app) => implementation 'com.github.medyo:android-about-page:1.3'
+        View aboutPage = new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+                .setDescription("A ATM consultoria tem como missão apoiar organizações que " +
+                        "desejam alcançar o sucesso através da excelência em gestão e da busca " +
+                        "pela qualidade.")
+                .addGroup("Entre em contato")
+                .addEmail("att@atmconsultoria.com.br", "att@atmconsultoria.com.br")
+                .addWebsite("https://atmconsultoria.com.br", "Acesse nosso site")
+                .addGroup("Redes sociais")
+                .addFacebook("andersonsprenger", "fb.com/andersonsprenger")
+                .addGitHub("knightencoder", "Github.com/knightencoder")
+                .addItem(versao)
+                .create();
+        return aboutPage;
     }
 }
